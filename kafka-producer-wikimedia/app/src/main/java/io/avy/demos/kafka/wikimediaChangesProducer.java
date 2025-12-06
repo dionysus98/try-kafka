81330138f7f3df32282871611bb694b5809991ba
Avinash S <avinashthebigboss@gmail.com>
2025-12-06 09:39:43 +0530

    init | proj | kafka-producer-wikimedia

---
+++ kafka-producer-wikimedia/app/src/main/java/io/avy/demos/kafka/wikimediaChangesProducer.java
@@ -0,0 +1,9 @@
+package io.avy.demos.kafka;
+
+
+
+public class wikimediaChangesProducer {
+    public static void main(String[] args) {
+        System.out.println("Hello!");                
+    }
+}
